0b3d627b848bc5d1821379d3a979c58eadfefc7c
niharika-annamraju <annamraju.n@northeastern.edu>
2022-10-08 12:49:44 +0530

    code push

---
+++ src/model/EmployeeHistory.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nihar
+ */
+public class EmployeeHistory {
+    private ArrayList<Employee> empHistory;
+    
+    public EmployeeHistory(){
+        this.empHistory = new ArrayList<Employee>();
+    }
+    
+    public ArrayList<Employee> getEmpHistory(){
+        return empHistory;
+    }
+    
+    public void setEmpHistory(ArrayList<Employee> empHistory){
+        this.empHistory = empHistory;
+    }
+    
+    public Employee addNewEmployee(){
+        Employee newEmployee = new Employee();
+        empHistory.add(newEmployee);
+        return newEmployee;
+    }
+    
+    public void deleteEmployee(Employee e){
+        empHistory.remove(e);
+    }
+}
